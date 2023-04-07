237a0f02597ffa6fd992b3cb72b2a242c49d5399
Shahadat Hosain <shahadat80@gmail.com>
2023-04-08 00:25:59 +0600

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,13 +32,13 @@ class HomeController extends Controller
         if(auth()->user()->hasAnyRole(['staff','admin'])):
             $dashboard['total-products'] = Product::whereNull('parent_id')->count();
             $dashboard['total-orders'] = Order::count();
-            $dashboard['total-clubs'] = $this->acList('club')->count();
-            $dashboard['total-teams'] = $this->acList('team')->count();
 /*
             for ($i = 6; $i >= 1; $i--) {
                 $months[] = date("M", strtotime( date( 'Y-m-01' )." -$i months"));
             }
 */
+
+/******* 
             $date1 = date('Y-m-1 00:00:00',strtotime( date( 'Y-m-01' )." -12 months"));
             $date2 = date('Y-m-31 23:59:59',strtotime( date( 'Y-m-01' )." -1 months"));
             $orders = Order::select(
@@ -62,7 +62,7 @@ class HomeController extends Controller
                 ->whereBetween('created_at', array($dates['first'], $dates['last']))
                 ->orderBy('created_at','asc')
                 ->get();
-
+*/
 /*
             $order_items = OrderItem::whereHas('order', function($q) use ($dates){
                 $q->whereBetween('updated_at', array($dates['first'], $dates['last']));//->where('status', 'Completed')
@@ -73,19 +73,24 @@ class HomeController extends Controller
             ->take(5)
             ->get();
 */
+
+/**************
         elseif(auth()->user()->hasAnyRole(['club'])):
             $teams = $this->teamList(auth()->user()->id);
             $dashboard['total-teams'] = $teams ? $teams->count() : 0;
             $players = $this->customerList(auth()->user()->id);
             $dashboard['total-players'] = $players ? $players->count() : 0;
-        endif;
+        
+        */
+endif;
+
 /*
         $sales = OrderItem::whereHas('roles', function($q){
             $q->where('slug', $acType);
         })->get();
 */
         //return $orders_last_month;
-        return view('admin.pages.dashboard',compact('dashboard','orders','order_items','orders_last_month'));
+        return view('admin.pages.dashboard',compact('dashboard'));//,'orders','order_items','orders_last_month'
 
     }
 }
--- resources/views/admin/order/order.blade.php
+++ resources/views/admin/order/order.blade.php
@@ -20,9 +20,6 @@
     <div class="card-body">
         {!! Form::model($data,['route'=>'order.index','method'=>'GET','class'=>'mb-3']) !!}
           <div class="row">
-            <div class="col-sm-2">{{ Form::select('club',$clubs,null,['class'=>'form-control select2','id'=>'club','placeholder'=>'All Club','data-url'=>route('teamApi')]) }}</div>
-            <div class="col-sm-2">{{ Form::select('team',$teams,null,['class'=>'form-control select2','id'=>'team','placeholder'=>'All Team']) }}</div>
-            <div class="col-sm-2">{{ Form::text('player_name',null,['class'=>'form-control','placeholder'=>'Player Name']) }}</div>
             <div class="col-sm-2">{{ Form::text('product_title',null,['class'=>'form-control','placeholder'=>'Product Title']) }}</div>
             <div class="col-sm-1">{{ Form::number('per_page',null,['class'=>'form-control','placeholder'=>'Product perpage']) }}</div>
             <div class="col-sm-3">
@@ -64,7 +61,7 @@
                     <td>#{{ $data->id }}</td>
                     <td><span data-toggle="tooltip" data-placement="top" title="{{ CustomHelper::prettyDateTime($data->created_at) }}">{{ CustomHelper::prettyDateS($data->created_at) }}</span></td>
                     <td>
-                        <span data-toggle="tooltip" data-placement="top" title="{{ $data->customer->team->club->name}} : {{ $data->customer->team->name }}">
+                        <span data-toggle="tooltip" data-placement="top" title="">
                             {{ $data->customer->first_name }} {{ $data->customer->last_name }}
                         </span>
                     </td>
@@ -124,21 +121,6 @@
 
     $(document).ready(function(){
         $('[data-toggle="tooltip"]').tooltip();
-    });
-
-    //$('#club').change(function(){
-    $('.card').on('load | change', '#club', function() {
-        $.get($(this).data('url'), {
-                option: $(this).val()
-        },
-        function(data) {
-                var subcat = $('#team');
-                subcat.empty();
-                subcat.append("<option value=''>All Team</option>")
-                $.each(data, function(index, element) {
-                        subcat.append("<option value='"+ element.id +"'>" + element.name + "</option>");
-                });
-        });
     });
 
     //Date range as a button
