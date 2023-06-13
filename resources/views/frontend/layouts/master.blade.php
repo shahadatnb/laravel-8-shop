08cc1c3acb3c75c347995a12e5c45119a877829f
sarkarpervez <sarkarpervez@gmail.com>
2023-06-13 14:57:25 +0600

    Header Cart Icom to Image, User Icaon To Image

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -111,13 +111,8 @@
                     </div>
                 </div>
                 <div class="col-md-1">
-                    <div class="userProfile">
-                        <a class="dropdown-toggle dropdownUser text-body-secondary text-decoration-none" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                            </svg>
-                        </a>
+                    <ul class="userProfile list-unstyled m-0 p-0">
+                        
                         @auth('customer')
                         <li class="nav-item dropdown">
                             <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> <img src="{{asset('/assets/front')}}/img/user.png" alt="User"> </a>
@@ -131,14 +126,14 @@
                             </ul>
                         </li>
                         @endauth
-                        <ul class="dropdown-menu">
+                        <ul class="dropdown-menu list-unstyled m-0 p-0">
                             @guest('customer')
                             <li><a class="dropdown-item" href="{{route('customer.login')}}">Sign In</a></li>
                             <li><a class="dropdown-item" href="{{route('customer.register')}}">Sign Up</a></li>
                             <li><a class="dropdown-item" href="#">Something else here</a></li>
                             @endguest
                         </ul>
-                    </div>
+                    </ul>
                 </div>
                 <div class="col-md-1">
                     <div class="wishCartList text-end">
@@ -155,9 +150,7 @@
                 <div class="col-md-1 text-end">
                     <div class="productCategories">
                         <span class="btnOffcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasHomeCart" aria-controls="offcanvasHomeCart">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                            </svg>
+                            <img src="assets\front\img\cart.png" class="img-fluid" alt="cart">
                         </span>
 
                         <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasHomeCart" aria-labelledby="offcanvasHomeCartLabel">
@@ -294,7 +287,7 @@
                 <div class="row justify-content-between align-items-center">
                     <div class="col-md-3 col-lg-2">
                         <img src="assets\front\img\Ladiumbd-logo-white.png" class="w-100" alt="Company Logo">
-                    </div>
+                    </div> 
                     <div class="col-md-5 col-lg-6">
                         <p>&copy; 2023. All Rights Reserved. Powered by {{ config('settings.appTitle','') }}.</p>
                     </div>
